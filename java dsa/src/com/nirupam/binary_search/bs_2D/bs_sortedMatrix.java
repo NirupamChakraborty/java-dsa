72fe8a5483cd05256d053b25d636cb5980db89f9
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2024-09-08 23:13:43 +0530

    BINARY SEARCH IN 2D MATRIX

---
+++ java dsa/src/com/nirupam/binary_search/bs_2D/bs_sortedMatrix.java
@@ -0,0 +1,89 @@
+package com.nirupam.binary_search.bs_2D;
+
+import java.util.Arrays;
+
+public class bs_sortedMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3,},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(Arrays.toString(search(arr,6)));
+    }
+// search in the row provided between the columns provided
+    static  int[] binarySearch(int[][] matrix, int row, int colStart, int colEnd, int target){
+        while(colStart <= colEnd){
+            int mid = colStart + (colEnd - colStart) / 2;
+            if (matrix[row][mid] == target) {
+                return new int[]{row,mid};
+            }
+            if (matrix[row][mid] < target){
+                 colStart = mid + 1;
+            }else {
+                 colEnd = mid - 1;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+    static int[] search(int[][] matrix, int target){
+        int rows = matrix.length;
+        int cols = matrix[0].length; // be cautious, matrix can be empty
+
+        if (rows == 1){
+            return binarySearch(matrix, 0,0,cols-1,target);
+        }
+
+
+        int rowStart = 0;
+        int rowEnd = rows -1;
+        int colMid = cols / 2; // total columns divide by 2
+
+
+//      run the loop till 2 rows are remaining
+        while(rowStart < (rowEnd - 1)){ // while this is true it will have more than 2 rows
+             int mid = rowStart + (rowEnd-rowStart) / 2;
+            if (matrix[mid][colMid] == target) {
+                   return  new int[]{mid, colMid};
+            }
+            if (matrix[mid][colMid] < target){
+                rowStart = mid;
+            }else {
+                rowEnd = mid;
+            }
+
+        }
+        //now we have two rows
+        //check whether the target is in the col of 2 rows
+        if (matrix[rowStart][colMid] == target){
+            return  new int[]{rowStart, colMid};
+        }
+        if (matrix[rowStart + 1][colMid] == target){
+            return  new int[]{rowStart + 1, colMid};
+        }
+        //otherwise
+        // search in 1st half
+        if(target <= matrix[rowStart][colMid - 1]){
+            return binarySearch(matrix, rowStart, 0, colMid-1, target);
+        }
+        // search in 2st half
+        if(target >= matrix[rowStart][colMid + 1] && target <= matrix[rowStart][cols - 1 ]){
+            return binarySearch(matrix, rowStart, colMid + 1, cols-1, target);
+
+        }
+        // search in 3st half
+        if(target <= matrix[rowStart + 1][colMid - 1]){
+            return binarySearch(matrix, rowStart + 1, 0, colMid - 1, target);
+
+        }
+        // search in 4st half
+//        if(target >= matrix[rowStart][colMid - 1]){
+//
+//        }
+        else {
+            return binarySearch(matrix, rowStart + 1, colMid + 1, cols-1, target);
+
+        }
+
+    }
+}
