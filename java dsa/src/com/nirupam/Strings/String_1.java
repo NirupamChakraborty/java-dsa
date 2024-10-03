6394e2931a88740fcc8523bfa189831b2cfcba2f
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2024-10-03 21:33:47 +0530

    Strings

---
+++ java dsa/src/com/nirupam/Strings/String_1.java
@@ -0,0 +1,9 @@
+package com.nirupam.Strings;
+
+public class String_1 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        String name = "Nirupam Chakraborty"; // collection of characters
+        System.out.println(name);
+    }
+}
